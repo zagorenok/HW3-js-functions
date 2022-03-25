9d2eae69b9ebc710a250169d250ba97acb8464cb
Victoria Ostapchuk <zagorenok#gmail.com>
2022-03-25 17:13:17 +0200

    first commit

---
+++ src/script.js
@@ -0,0 +1,71 @@
+console.log('#4');
+
+function calcSum(a, b) {
+  return a + b;
+}
+console.log(calcSum(3, 7));
+
+function calcDifference(a, b) {
+  return a - b;
+}
+console.log(calcDifference(5, 3));
+
+function calcMultiplication(a, b) {
+  return a * b;
+}
+console.log(calcMultiplication(5, 5));
+
+function calcDivision(a, b) {
+  return a / b;
+}
+console.log(calcDivision(8, 2));
+
+console.log('#5');
+
+function showNumbers(n) {
+  for (let i = 1; i <= n; i++) {
+console.log(i);  
+  }
+}
+showNumbers(5);
+
+console.log('#6');
+
+function showNumber(n) {
+  for (let i = 5; i >= 1; i--) {
+console.log(i);  
+  }
+}
+showNumber(5);
+
+console.log('#7');
+
+function calcPow(x, n) {
+  return;
+}
+calcPow(5, 3);
+console.log(Math.pow(5, 3));
+
+console.log('#8');
+
+function isBigger(a, b) {
+  if ( a > b) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(isBigger(5, -1));
+
+
+console.log('#9');
+
+function isSmaller(a, b) {
+  if ( a < b) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(isSmaller(5, -1));
+
